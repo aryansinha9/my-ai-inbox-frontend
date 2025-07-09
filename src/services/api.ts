d16d11ef17a3ace9821212ee43ecf28697c77a1d
Aryan Sinha <z5589770@ad.unsw.edu.au>
2025-07-09 16:04:10 +1000

    feat: Parameterize API URL for deployment

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:4000/api'; // Your backend URL
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; // Your backend URL
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
